84e8b28b2a21ecef15e06acbccb29404b826985d
Redon Qerimi <redon.qerimi@hotelkeyapp.com>
2023-12-08 22:12:21 +0100

    finish login page

--- js/login.js
+++ js/login.js
@@ -1,26 +1,47 @@
 let emailInput = document.getElementById('email');
 let passwordInput = document.getElementById('password');
+let errorMsgSpan = document.getElementsByClassName('err-msg')[0]
 
-function login() {
-    if (emailInput.value === "") {
+function login(e) {
+    e.preventDefault()
+
+    if (emailInput.value === "" || passwordInput.value === "") {
         emailInput.focus();
-        return alert("Ju lutem shenoni nje email.")
+        return showErrorMsg('both',"Ju lutem shenoni te gjitha te dhenat.")
     }
 
-    if (passwordInput.value === "") {
-        passwordInput.focus();
-        return alert("Ju lutem shenoni nje password.")
-    }
-
-    if(!emailIsValid(emailInput.value)) {
-        emailInput.focus()
-        return alert("Emaili i shenuar nuk eshte valid.")
+    if (emailInput.value === "" || !emailIsValid(emailInput.value)) {
+        emailInput.focus();
+        return showErrorMsg('email',"Ju lutem shenoni nje email valide.")
     }
 
     if (!passwordIsValid(passwordInput.value)) {
         passwordInput.focus()
-        return alert("Passwordi duhet te jete mbi 6 karaktere.")
+        return showErrorMsg('password', "Passwordi duhet te jete mbi 6 karaktere.")
     }
+
+
+
+    errorMsgSpan.innerHTML = "";
+    passwordInput.style.border = '1px solid';
+    emailInput.style.border = '1px solid';
+    window.location.href = "../success.html";
+}
+
+function showErrorMsg(inputType, msg) {
+
+    if (inputType === 'email') {
+        emailInput.style.border = '1px solid red';
+        passwordInput.style.border = '1px solid';
+    } else if (inputType === 'password') {
+        emailInput.style.border = '1px solid';
+        passwordInput.style.border = '1px solid red';
+    } else {
+        passwordInput.style.border = '1px solid red';
+        emailInput.style.border = '1px solid red';
+    }
+
+    errorMsgSpan.innerHTML = msg;
 }
 
 function emailIsValid(email) {
