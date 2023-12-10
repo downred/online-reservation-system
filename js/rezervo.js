4f777b297bea1fa561946d199f293fd57796e894
lummeta25 <lummeta25@gmail.com>
2023-12-10 20:00:21 +0100

    fixed bug for form to dissapear after pressing rezervo

---
+++ js/rezervo.js
@@ -0,0 +1,31 @@
+function DisplayForm(){
+    popup.classList.add("popup-show");
+}
+function CloseForm(){
+    popup.classList.remove("popup-show");
+}
+function SubmitFrom(){
+    let startDate = document.getElementById("StartDate").value
+    let endDate = document.getElementById("EndDate").value
+    let msg = document.getElementById("error-msg")
+    
+    if(startDate>endDate){
+        msg.textContent = "Data e fillimit te rezervimit duhet te jete me e vogel se ajo e mbarimit!"
+        msg.classList.add("date-error")
+        console.log("date error")
+        
+    }else{
+        msg.textContent = "Rezervimi u krye me sukses!"
+        msg.classList.remove("date-error")
+        msg.classList.add("success-msg")
+        setTimeout(() => {
+            popup.classList.remove("popup-show");
+            msg.classList.remove("success-msg")
+        }, 5000) 
+    }
+
+    event.preventDefault();
+    let popup = document.getElementById("popup")
+    console.log(startDate)
+    console.log(endDate) 
+}
