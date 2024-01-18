67a08d8d0cd041419f914f8c96a3538d792fd3c3
downred <redonqerimi@gmail.com>
2024-01-18 09:43:28 +0100

    start working on admin dashboard

---
+++ dashboard/dboard-rezervimet.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="navbar-container">
+            <?php include 'navbar.php' ?>
+        </div>
+        <div class="entry-container">
+            <div class="entry">
+                <p>Filan Fisteku ka kryer rezervim ne Hotelin 1 nga data ... deri ...</p>
+            </div>
+            <div class="entry">
+                <p>Filan Fisteku ka kryer rezervim ne Hotelin 1 nga data ... deri ...</p>
+            </div>
+            <div class="entry">
+                <p>Filan Fisteku ka kryer rezervim ne Hotelin 1 nga data ... deri ...</p>
+            </div>
+            <div class="entry">
+                <p>Filan Fisteku ka kryer rezervim ne Hotelin 1 nga data ... deri ...</p>
+            </div>
+            <div class="entry">
+                <p>Filan Fisteku ka kryer rezervim ne Hotelin 1 nga data ... deri ...</p>
+            </div>
+            <div class="entry">
+                <p>Filan Fisteku ka kryer rezervim ne Hotelin 1 nga data ... deri ...</p>
+            </div>
+            <div class="entry">
+                <p>Filan Fisteku ka kryer rezervim ne Hotelin 1 nga data ... deri ...</p>
+            </div>
+            <div class="entry">
+                <p>Filan Fisteku ka kryer rezervim ne Hotelin 1 nga data ... deri ...</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+<style>
+    @import 'main.css';
+
+    .wrapper {
+        display: flex;
+        height: 100%;
+    }
+
+    .entry-container {
+        width: 100%;
+        height: min-content;
+        display: flex;
+        margin: 1rem 0 0 250px;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .entry {
+        width: 60%;
+        padding: 2rem;
+        margin: 0 0 1rem 0;
+        border-radius: 20px;
+        background-color: var(--color-light-lighter);
+    }
+
+    .header-container {
+        width: 100%;
+    }
+
+    h1 {
+        text-align: left;
+    }
+</style>
---
+++ dashboard/navbar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="navbar-wrapper">
+        <div class="profile">
+            <div class="profile-img-container">
+                <img src="../images/icons/profile.png" alt="pfp">
+            </div>
+            <div class="profile-name">
+                <span>Admin</span>
+            </div>
+        </div>
+        <div class="links">
+            <ul>
+                <li><a href="dboard-rezervimet.php">Rezervimet</a></li>
+                <li><a href="dboard-hotelet.php">Hotelet</a></li>
+                <li><a href="dboard-mesazhet.php">Mesazhet</a></li>
+                <li><a href="dboard-news.php">Te rejat</a></li>
+                <li><a href="dboard-logout.php">Çkyçu</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
