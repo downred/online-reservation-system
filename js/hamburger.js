c0b0d4b638e6b947aec28bc77dae1e05a07cdc06
downred <redonqerimi@gmail.com>
2024-01-30 21:24:56 +0100

    add hamburger to navbar for smaller displays

---
+++ js/hamburger.js
@@ -0,0 +1,21 @@
+let hamburger = document.getElementById("hamburger");
+let hamburgerContainer = document.getElementsByClassName("mobile-links")[0];
+let body = document.getElementsByTagName('body')[0]
+
+hamburger.onclick = function (e) {
+    e.preventDefault();
+    console.log(body)
+
+  let containerDisplay = window
+    .getComputedStyle(hamburgerContainer)
+    .getPropertyValue("display");
+
+  if (containerDisplay === "none" || containerDisplay === "") {
+      hamburgerContainer.style.display = "block";
+      body.style.overflow = "hidden";
+
+  } else {
+      hamburgerContainer.style.display = "none";
+      body.style.overflow = "";
+  }
+};
--- nav-bar.php
+++ nav-bar.php
@@ -1,11 +1,14 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<body>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
     <nav>
         <ul class="nav-container">
             <div class="nav-section-one">
@@ -34,17 +37,63 @@
             <li class="nav-section-two">
                 <a class="btn-light m-2" href="login.php">Kyçu</a>
                 <?php
-                session_start();
                 if (isset($_SESSION["userid"])) {
                     echo '<a class="btn-light m-2" href="logout.php">Çkyçu</a>';
                 }
                 ?>
-                
+
             </li>
         </ul>
+        <div class="mobile-navbar">
+            <div class="img-container">
+                <img src="./images/logo/logo.png" alt="" width="70px">
+            </div>
+            <div class="hamburger">
+                <a id="hamburger" href="">
+                    <img src="./images/icons/hamburger.png" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="mobile-links">
+            <div class="link-container">
+                <div>
+                    <a href="index.php">Ballina</a>
+                </div>
+
+                <div>
+                    <a href="rezervo.php">Hotelet</a>
+                </div>
+
+                <div>
+                    <a href="rreth-nesh.php">Rreth nesh</a>
+                </div>
+
+                <div>
+                    <a href="news.php">Të rejat</a>
+                </div>
+
+                <div>
+                    <a href="FAQ.php">FAQ</a>
+                </div>
+
+                <div>
+                    <a href="kontakt.php">Na kontaktoni</a>
+                </div>
+
+                <div>
+                    <a class="btn-light" href="login.php">Kyçu</a>
+                    <?php
+                    if (isset($_SESSION["userid"])) {
+                        echo '<a class="btn-light" href="logout.php">Çkyçu</a>';
+                    }
+                    ?>
+                </div>
+
+            </div>
+        </div>
     </nav>
 
-<body>
+    <script src="./js/hamburger.js"></script>
 
 </body>
 
